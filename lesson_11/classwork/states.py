21d58cf35f6530f0ee58a5b5f1aefe09d5f51b3e
Julia-Shejko <Julia_Shejko@i.ua>
2022-11-28 23:40:00 +0200

    Upload lesson_11 classwork

---
+++ lesson_11/classwork/states.py
@@ -0,0 +1,53 @@
+import random
+from dataclasses import dataclass, field
+from enum import Enum, auto
+
+
+def random_delay():
+    return random.random() * 3
+
+
+def random_countdown():
+    return random.randint(1, 5)
+
+
+class Status(Enum):
+    NOT_STARTED = auto()
+    STARTED = auto()
+    FINISHED = auto()
+
+
+class Operations(Enum):
+    WAIT = auto()
+    STOP = auto()
+
+
+@dataclass
+class Rocket:
+    status: Status = Status.NOT_STARTED
+    delay: float = field(default_factory=random_delay)
+    countdown: int = field(default_factory=random_countdown)
+
+    def step(self):
+        if self.status is Status.NOT_STARTED:
+            self.status = Status.STARTED
+            return Operations.WAIT, self.delay
+        if self.status is Status.STARTED:
+            if self.countdown == 0:
+                self.status = Status.FINISHED
+            else:
+                self.countdown -= 1
+                return Operations.WAIT, 1
+        if self.status is Status.FINISHED:
+            return Operations.STOP, None
+
+
+def main():
+    print("Hello")
+
+
+# run_sync()
+
+
+if __name__ == "__main__":
+    main()
---
+++ lesson_11/classwork/threads_example.py
@@ -0,0 +1,23 @@
+from threading import Thread
+from time import perf_counter, sleep
+
+
+def foo(message: str, delay: int):
+    for _ in range(5):
+        sleep(delay)
+        print(message)
+
+
+thread_1 = Thread(target=foo, args=["I still working", 1])
+thread_2 = Thread(target=foo, args=["Another message", 1])
+
+start_time = perf_counter()
+
+thread_1.start()
+thread_2.start()
+
+thread_1.join()
+thread_2.join()
+
+end_time = perf_counter()
+print(end_time - start_time)
